b18f51ab1d85c49fe5e0ab327ad1d7c5b5cf0d91
Gimond <pierre@gimond.fr>
2014-05-28 10:49:45 +0200

    maj konami code

--- js/general.js
+++ js/general.js
@@ -1,13 +1,25 @@
 jQuery('document').ready(function ($) {
 
 	// konami code
-	if (window.addEventListener) {
+	if ( window.addEventListener ) {
 		var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
-		window.addEventListener("keydown", function (e) {
-			kkeys.push(e.keyCode);
-			if (kkeys.toString().indexOf(konami) >= 0) {
+		window.addEventListener("keydown", function(e){
+			kkeys.push( e.keyCode );
+			if ( kkeys.toString().indexOf( konami ) >= 0 ){
 
-				alert('KONAMI CODE ACTIVÉ!');
+				$('img').each(function(index, el) {
+					$(this).attr('src', template_url+'/images/sainty.jpg').mouseenter(function() {
+						if (!$(this).data('hey')){
+							$(this).data('hey', true);
+							$(this).attr('src', template_url+'/images/sainty_hey.jpg');
+							var sainty = $(this);
+							setTimeout(function(){
+								sainty.attr('src', template_url+'/images/sainty.jpg');
+								sainty.data('hey', false);
+							}, 200);
+						}
+					});
+				});
 
 				kkeys = [];
 			}
