71d28bc356617969a9749fb5b9747d2edd0a2111
Gimond <pierre@gimond.fr>
2014-09-24 11:56:12 +0200

    menu_icon
    utilisation du paramètre menu_icon pour les cpt

--- inc/custom_post_types.php
+++ inc/custom_post_types.php
@@ -1,4 +1,6 @@
 <?php
+// le paramètre "menu_icon" est le nom de l'icône
+// Toutes les icônes avec leur noms sont ici => http://melchoyce.github.io/dashicons/
 function create_post_type() {
 	// PROJETS
 	register_post_type( 'projet',
@@ -25,17 +27,3 @@ function create_post_type() {
 	);
 }
 add_action( 'init', 'create_post_type' );
-
-// ICONES
-// à choper ici => http://melchoyce.github.io/dashicons/
-function cpt_icons() {
-    ?>
-    <style type="text/css" media="screen">
-		/* PROJETS */
-        #adminmenu .menu-icon-projet div.wp-menu-image:before {
-          content: '\f174';
-        }
-    </style>
-	<?php
-}
-add_action( 'admin_head', 'cpt_icons' );
